f2221ba899b9a7b8c63fffe94a0ecd0ff75ec682
Maxime Boudreau <maxime.boudreau@umoncton.ca>
2017-02-12 15:08:29 -0400

    Commit just before creation of the UpdateLCD function

--- LCD_Strob/LCD_Strob.ino
+++ LCD_Strob/LCD_Strob.ino
@@ -6,7 +6,7 @@
 
 This program will test the LCD panel and the buttons
 Mark Bramwell, July 2010
-lollllllll
+
 ********************************************************/
 
 // select the pins used on the LCD panel
@@ -20,15 +20,10 @@ int curPos = 4; //position décimale par défaut du curseur
 double freq = 1; //Fréquence par défaut du stroboscope
 int buttonDelay = 120;
 
-int pos1Val = 0;
-int pos2Val = 0;
-int pos3Val = 0;
-int pos4Val = 1;
-int pos5Val = 0;
-int pos6Val = 0;
 bool buttonReady = true;
 
 double incVal = 1;
+int displayMode = 0;
 
 #define btnRIGHT  0
 #define btnUP     1
@@ -37,20 +32,22 @@ double incVal = 1;
 #define btnSELECT 4
 #define btnNONE   5
 
-// read the buttons
+#define mFreq 0
+#define mDuty 1
 
-char x[10];
 
-void updateFreq(String dir)
+
+void updateFreq(String dir) // Increment frequency
 {
   Serial.println(String(freq)); //For debugging
   float newFreq = freq;
   String freqString;
   if (dir == "+") newFreq = freq + incVal; //increase or decrease frequency
   else if (dir == "-") newFreq = freq - incVal;
- 
 
-  if ((newFreq >= 0) && (newFreq <= 10000)) freq=newFreq; //
+  if ((newFreq >= 0) && (newFreq <= 10000)) freq=newFreq; //Make sure the new 
+  //frequency remains within accepted boundary. 
+  
 
   //Printing freq on lcd
   freqString = String(freq,1);
@@ -62,29 +59,23 @@ void updateFreq(String dir)
   for (int i=0;i<fLength;i++) {
     invFreqString += freqString[fLength-1-i];
   }
-  //Serial.println(invFreqString);
   while (invFreqString.length() < 7){
     invFreqString +=" "; //Extra space
   }
   lcd.print(invFreqString);
   lcd.leftToRight();
   
-  dtostrf(newFreq, 3, 2, x);
-  Wire.beginTransmission(8); // transmit to device #8
-  Wire.write(x);        
-  Wire.endTransmission();    // stop transmitting
   
 }
 
 
-void changeincVal(String dir)
+void changeincVal(String dir) //This function set the decimal position of the increment (between 0.1 and 1000)
 {
   if (dir == "right") {
     Serial.println("1");
     if (incVal == 0.1) return;
     else {
       incVal = incVal / 10;
-      //lcd.blink(1,5);
     }
   }
   if (dir == "left") {
@@ -93,111 +84,71 @@ void changeincVal(String dir)
     else {
       incVal = incVal * 10;
     }
-
-
   }
   delay(buttonDelay * 2);
 }
 
-int read_LCD_buttons()
-{
-  adc_key_in = analogRead(0);      // read the value from the sensor
-  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
-  // we add approx 50 to those values and check to see if we are close
-  if (adc_key_in > 1000) {
-    return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
-  }
-
-
-  if (adc_key_in < 50)   {
-
-    return btnRIGHT;
-  }
-  if (adc_key_in < 195)  return btnUP;
-  if (adc_key_in < 380)  return btnDOWN;
-  if (adc_key_in < 555)  {
-
-    return btnLEFT;
-  }
-  if (adc_key_in < 790)  return btnSELECT;
-
-
-
-  return btnNONE;  // when all others fail, return this...
-}
 
 
 void setup()
 {
-
-// Setup du générateur
- pinMode(10, OUTPUT);
- digitalWrite(10,HIGH);
- //Timer1.initialize(150000);
- //Timer1.pwm(10, 12, 15000);
- 
-
-
   lcd.begin(16, 2);              // start the library
+
+  //Intro display of the LCD 
   lcd.setCursor(0, 0);
-  lcd.print("Strob. par MxBoud!"); // print a simple message
+  lcd.print("Strob. by  MxBoud"); // print a simple message
   delay(1000);
+  
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("enjoy :)");
+  lcd.print("life's good.");
   Serial.begin(9600);
-  Wire.begin();
+  //Wire.begin();
 }
 
 void loop()
 {
 
+
+  lcd_key = readLCDcontroller();  // read the buttons
+  respondToButtonInput(); //Do what the button is supposed to do
+
+}
+int readLCDcontroller() //Tells wich button is selected
+{
+  adc_key_in = analogRead(0);      // read the value from the sensor
+  // we add approx 50 to those values and check to see if we are close
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  if (adc_key_in < 50)   return btnRIGHT;
+  if (adc_key_in < 195)  return btnUP;
+  if (adc_key_in < 380)  return btnDOWN;
+  if (adc_key_in < 555)  return btnLEFT;
+  if (adc_key_in < 790)  return btnSELECT;
+  return btnNONE;  // when all others fail, return this...
+}
+
+void respondToButtonInput() //Respond to current lcd_key and initiate corresponding callback.
+{
  
-
-  
-  lcd.setCursor(9, 0);
-  lcd.print(String(25, HEX));
-
-  
-
-
-//  lcd.setCursor(9, 1);           // move cursor to second line "1" and 9 spaces over
-//  lcd.print(String(freq));      // display seconds elapsed since power-up
-
-  updateFreq("");
-
-
-  lcd.setCursor(0, 1);           // move to the begining of the second line
-  lcd_key = read_LCD_buttons();  // read the buttons
- 
-
-
-
-
-  switch (lcd_key)               // depending on which button was pushed, we perform an action
+    switch (lcd_key)               // depending on which button was pushed, we perform an action
   {
     case btnRIGHT:
       {
-
-        bool buttonReady = false;
         changeincVal("right");
+        lcd.setCursor(0,1);
         lcd.print(String(incVal));
-
-
-
         break;
       }
     case btnLEFT:
       {
-        bool buttonReady = false;
         changeincVal("left");
+        lcd.setCursor(0,1);
         lcd.print(String(incVal));
         break;
       }
     case btnUP:
       {
         lcd.print("UP    ");
-
         updateFreq("+");
         delay(buttonDelay);
         break;
@@ -205,24 +156,34 @@ void loop()
     case btnDOWN:
       {
         lcd.print("DOWN  ");
-
         updateFreq("-");
         delay(buttonDelay);
         break;
       }
-    case btnSELECT:
+    case btnSELECT: // Switch between frequency mode and dutycycle mode
       {
-        lcd.print("SELECT");
+        lcd.setCursor(0,0);
+        
+        if (displayMode == mFreq) {
+          displayMode = mDuty;
+          lcd.print("Duty cycle");
+        }
+        else if (displayMode == mDuty) {
+          displayMode = mFreq;
+          lcd.print("Frequency");
+          
+        }
+        delay(buttonDelay);
+        
         break;
       }
     case btnNONE:
       {
-
+        lcd.setCursor(0,1);
         lcd.print(String(incVal));
         break;
       }
   }
-
 
 }
 
