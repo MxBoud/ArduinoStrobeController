7291119beca901d2a9868826e3d92a0f7d98276b
Maxime Boudreau <maxime.boudreau@umoncton.ca>
2017-02-12 20:10:37 -0400

    First working version of the project.

--- LCD_Strob/LCD_Strob.ino
+++ LCD_Strob/LCD_Strob.ino
@@ -3,26 +3,33 @@
 #include <Wire.h>
 
 /*******************************************************
+This program is used with a DFRobot LCD keypad shield. It implements a simple UI for setting values to 
+the variable freq and duty through the buttons of the shield. 
 
-This program will test the LCD panel and the buttons
-Mark Bramwell, July 2010
+Code written by Maxime Boudreau (February 2017) using the example code from Mark Bramwell (July 2010)
 
 ********************************************************/
+
+//Initial values for the ajustable variables. 
+double freq = 1; //1 Hz
+double duty = 50;// 50%
+
+int lcd_key     = 0;
+int adc_key_in  = 0;
+
 
 // select the pins used on the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 // define some values used by the panel and buttons
-int lcd_key     = 0;
-int adc_key_in  = 0;
 
-int curPos = 4; //position décimale par défaut du curseur
-double freq = 1; //Fréquence par défaut du stroboscope
-int buttonDelay = 120;
 
-bool buttonReady = true;
 
-double incVal = 1;
+int buttonDelay = 120; //Speed at wich the Arduino respond to user entry.
+
+double incValF = 1;
+double incValD = 1;
+
 int displayMode = 0;
 
 #define btnRIGHT  0
@@ -31,89 +38,65 @@ int displayMode = 0;
 #define btnLEFT   3
 #define btnSELECT 4
 #define btnNONE   5
-
-#define mFreq 0
-#define mDuty 1
-
-
-
-void updateFreq(String dir) // Increment frequency
-{
-  Serial.println(String(freq)); //For debugging
-  float newFreq = freq;
-  String freqString;
-  if (dir == "+") newFreq = freq + incVal; //increase or decrease frequency
-  else if (dir == "-") newFreq = freq - incVal;
-
-  if ((newFreq >= 0) && (newFreq <= 10000)) freq=newFreq; //Make sure the new 
-  //frequency remains within accepted boundary. 
-  
-
-  //Printing freq on lcd
-  freqString = String(freq,1);
-  lcd.setCursor(15,1);
-  lcd.rightToLeft();
-  //Right justify
-  int fLength = freqString.length();
-  String invFreqString = "";
-  for (int i=0;i<fLength;i++) {
-    invFreqString += freqString[fLength-1-i];
-  }
-  while (invFreqString.length() < 7){
-    invFreqString +=" "; //Extra space
-  }
-  lcd.print(invFreqString);
-  lcd.leftToRight();
-  
-  
-}
-
-
-void changeincVal(String dir) //This function set the decimal position of the increment (between 0.1 and 1000)
-{
-  if (dir == "right") {
-    Serial.println("1");
-    if (incVal == 0.1) return;
-    else {
-      incVal = incVal / 10;
-    }
-  }
-  if (dir == "left") {
-    Serial.println("0");
-    if (incVal == 1000) return;
-    else {
-      incVal = incVal * 10;
-    }
-  }
-  delay(buttonDelay * 2);
-}
-
+#define mFreq     0
+#define mDuty     1
 
 
 void setup()
 {
   lcd.begin(16, 2);              // start the library
 
-  //Intro display of the LCD 
+  //Intro display of the LCD
   lcd.setCursor(0, 0);
-  lcd.print("Strob. by  MxBoud"); // print a simple message
+  lcd.print("Strob. by MxBoud");
   delay(1000);
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("life's good.");
+
   Serial.begin(9600);
-  //Wire.begin();
+  updateLCD();
+
 }
 
 void loop()
 {
-
-
   lcd_key = readLCDcontroller();  // read the buttons
   respondToButtonInput(); //Do what the button is supposed to do
-
 }
+
+void changeincVal(String dir) //This function set the decimal position of the increment (between 0.1 and 1000)
+{
+  if (displayMode == mFreq)
+  {
+    if (dir == "left") {
+      if (incValF == 0.1) return;
+      else {
+        incValF = incValF / 10;
+      }
+    }
+    if (dir == "right") {
+      if (incValF == 1000) return;
+      else {
+        incValF = incValF * 10;
+      }
+    }
+  }
+  else if (displayMode == mDuty)
+  {
+    if (dir == "right") {
+      if (incValD == 0.1) return;
+      else {
+        incValD = incValD / 10;
+      }
+    }
+    if (dir == "left") {
+      if (incValD == 100) return;
+      else {
+        incValD = incValD * 10;
+      }
+    }
+    delay(buttonDelay * 2);
+  }
+}
+
 int readLCDcontroller() //Tells wich button is selected
 {
   adc_key_in = analogRead(0);      // read the value from the sensor
@@ -129,60 +112,146 @@ int readLCDcontroller() //Tells wich button is selected
 
 void respondToButtonInput() //Respond to current lcd_key and initiate corresponding callback.
 {
- 
-    switch (lcd_key)               // depending on which button was pushed, we perform an action
+
+  switch (lcd_key)               // depending on which button was pushed, we perform an action
   {
     case btnRIGHT:
       {
         changeincVal("right");
-        lcd.setCursor(0,1);
-        lcd.print(String(incVal));
+        updateLCD();
+        delay(buttonDelay*2);
         break;
       }
     case btnLEFT:
       {
         changeincVal("left");
-        lcd.setCursor(0,1);
-        lcd.print(String(incVal));
+        updateLCD();
+        delay(buttonDelay*2);
         break;
       }
     case btnUP:
       {
-        lcd.print("UP    ");
-        updateFreq("+");
+        updateFreqDuty("+");
+        updateLCD();
         delay(buttonDelay);
         break;
       }
     case btnDOWN:
       {
-        lcd.print("DOWN  ");
-        updateFreq("-");
+        updateFreqDuty("-");
+        updateLCD();
         delay(buttonDelay);
         break;
       }
     case btnSELECT: // Switch between frequency mode and dutycycle mode
       {
-        lcd.setCursor(0,0);
-        
         if (displayMode == mFreq) {
           displayMode = mDuty;
-          lcd.print("Duty cycle");
         }
         else if (displayMode == mDuty) {
           displayMode = mFreq;
-          lcd.print("Frequency");
-          
         }
-        delay(buttonDelay);
-        
+        updateLCD();
+        delay(4 * buttonDelay);
+
         break;
       }
     case btnNONE:
       {
-        lcd.setCursor(0,1);
-        lcd.print(String(incVal));
+        //updateLCD();
         break;
       }
+  }
+
+}
+
+void updateFreqDuty(String dir) // Increment frequency or DutyCycle
+{
+  if (displayMode == mFreq) {
+    float newFreq = freq;
+    if (dir == "+") newFreq = freq + incValF; //increase or decrease frequency
+    else if (dir == "-") newFreq = freq - incValF;
+
+    if ((newFreq >= 0.01) && (newFreq <= 10000)) freq = newFreq; //Make sure the new
+
+    float period = (1/freq)*1e6; //Period in microseconds
+    
+    Serial.println("P"+String(period));
+    //frequency remains within accepted boundary.
+  }
+  if (displayMode == mDuty) {
+    float newDuty = duty;
+    if (dir == "+") newDuty = duty + incValD; //increase or decrease duty
+    else if (dir == "-") newDuty = duty - incValD;
+
+    if ((newDuty >= 0.01) && (newDuty <= 99)) duty = newDuty; //Make sure the new
+    //duty remains within accepted boundary.
+    
+    Serial.println("D"+String(duty/100*1023));
+  }
+}
+
+void updateLCD() 
+{
+  lcd.clear();
+
+  //Display mode
+  lcd.setCursor(0, 0);
+  if (displayMode == mFreq) {
+    lcd.print("Frequency (Hz)");
+  }
+  else if (displayMode == mDuty) {
+    lcd.print("Duty cycle (%)");
+  }
+
+  //Increment
+  lcd.setCursor(0, 1);
+  if (displayMode == mFreq) {
+    lcd.print("I=" + String(incValF));
+  }
+
+  else if (displayMode == mDuty) {
+    lcd.print("I=" + String(incValD));
+  }
+
+
+  //Current freq/duty
+
+  if (displayMode == mFreq) {
+    String freqString;
+    freqString = String(freq, 1);
+    lcd.setCursor(15, 1);
+    lcd.rightToLeft();
+    //Right justify
+    int fLength = freqString.length();
+    String invFreqString = "";
+    for (int i = 0; i < fLength; i++) {
+      invFreqString += freqString[fLength - 1 - i];
+    }
+    while (invFreqString.length() < 7) {
+      invFreqString += " "; //Extra space
+    }
+    lcd.print(invFreqString);
+    lcd.leftToRight();
+  }
+
+  else if (displayMode == mDuty) {
+
+    String dutyString;
+    dutyString = String(duty, 1);
+    lcd.setCursor(15, 1);
+    lcd.rightToLeft();
+    //Right justify
+    int dLength = dutyString.length();
+    String invDutyString = "";
+    for (int i = 0; i < dLength; i++) {
+      invDutyString += dutyString[dLength - 1 - i];
+    }
+    while (invDutyString.length() < 7) {
+      invDutyString += " "; //Extra space
+    }
+    lcd.print(invDutyString);
+    lcd.leftToRight();
   }
 
 }
---
+++ PWM/PWM.ino
@@ -0,0 +1,74 @@
+/*
+ *  Timer1 library example
+ *  June 2008 | jesse dot tane at gmail dot com
+ */
+
+#include "TimerOne.h"
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+double period = 1000000; 
+double duty = 511;
+
+void setup()
+{
+  Serial.begin(9600);
+  inputString.reserve(200);
+  pinMode(10, OUTPUT);
+  Timer1.initialize(500000);         // initialize timer1, and set a 1/2 second period
+  Timer1.pwm(9, duty,period);                // setup pwm on pin 9, 50% duty cycle
+}
+
+
+void loop()
+{
+  // your program here...
+  serialEvent();
+  if (stringComplete) {
+    Serial.println(inputString); //Input must be in format as in 
+    //these examples: D50, D1.1, F10, F1000
+
+    char mode = inputString[0];
+    inputString.remove(0,1);
+    float value = inputString.toFloat();
+    
+    if (mode == 'D'){
+      duty = value;
+      Serial.println(value);  
+    }
+    else if (mode = 'P') {
+      period = value;
+      Serial.println(value);
+    }
+    Timer1.pwm(9,duty,period);
+    inputString = "";
+    stringComplete = false;
+  }
+   
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    
+    
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+      
+    }
+    else {
+      inputString += inChar;
+    }
+  }
+}
